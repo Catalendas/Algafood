eecd1891b199be9fef9aad2096279e431a31c71d
catalendas <musiccar@outlook.com>
2026-04-10 10:49:55 -0300

    fix: remove AuthenticationManagerBuilder

--- src/main/java/com/catalendas/algafood/core/security/WebSecurityConfig.java
+++ src/main/java/com/catalendas/algafood/core/security/WebSecurityConfig.java
@@ -7,8 +7,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -27,11 +31,15 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("marcos")
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("marcos")
                 .password(passwordEncoder().encode("1234"))
-                .roles("ADMIN");
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
