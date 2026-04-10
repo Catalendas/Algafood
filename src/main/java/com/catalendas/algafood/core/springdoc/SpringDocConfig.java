68cb85198f0ea65538b545cc3c02b80daef1c468
catalendas <musiccar@outlook.com>
2026-04-10 16:54:22 -0300

    create: SpringDocConfig

---
+++ src/main/java/com/catalendas/algafood/core/springdoc/SpringDocConfig.java
@@ -0,0 +1,39 @@
+package com.catalendas.algafood.core.springdoc;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(name = "security_auth", type = SecuritySchemeType.OAUTH2,
+    flows = @OAuthFlows(authorizationCode = @OAuthFlow(authorizationUrl = "${springdoc.oAuthFlow.authorizationUrl}",
+            tokenUrl = "${springdoc.oAuthFlow.tokenUrl}",
+            scopes = {
+                @OAuthScope(name = "READ", description = "read scope"),
+                @OAuthScope(name = "WRITE", description = "write scope")
+            }
+    )))
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                    .title("AlgaFood API")
+                    .version("V1")
+                    .description("Rest API do AlgaFood")
+                        .license(new License()
+                                .name(" Apache 2.0")
+                                .url("https://springdoc.com")));
+    }
+
+
+}
